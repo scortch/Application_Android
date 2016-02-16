bedefd8405d4eedf63777c3465d8b8e342c154e0
scortch <julien.martin.999@gmail.com>
2016-02-16 17:06:29 +0100

    Refactor suite

--- Boit'J/app/src/main/java/com/androidbelieve/materialnavigationdrawer/FormulaireActivity.java
+++ Boit'J/app/src/main/java/com/androidbelieve/materialnavigationdrawer/FormulaireActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class FormulaireActivity extends FragmentActivity {
+public class FormulaireActivity extends AppCompatActivity {
     private EditText subject;
     private EditText body;
 
