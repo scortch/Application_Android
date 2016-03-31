738ca56ea5eb8c4192c3d57eb697f10f378703dd
scortch <julien.martin.999@gmail.com>
2016-03-31 17:15:19 +0200

    création fragment Mentions Légales

---
+++ Boit'J/app/src/main/java/com/androidbelieve/materialnavigationdrawer/LegalFragment.java
@@ -0,0 +1,28 @@
+package com.androidbelieve.materialnavigationdrawer;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LegalFragment extends Fragment {
+
+
+    public LegalFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.legal_layout, container, false);
+        return view;
+    }
+
+}
